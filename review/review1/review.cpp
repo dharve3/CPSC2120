62084925a389106efcc868e4a05f9cfda0363a3a
D-Guy Laptop <ddcool535@gmail.com>
2024-09-02 19:11:05 -0400

    Added inital review1 files

---
+++ review/review1/review.cpp
@@ -0,0 +1,8 @@
+#include "review.h"
+
+using namespace std;
+
+void ReadStdIn() {
+    string s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+}
---
+++ review/review1/testerMain.cpp
@@ -0,0 +1,7 @@
+#include "review.h"
+
+using namespace std;
+
+int main() {
+    
+}
