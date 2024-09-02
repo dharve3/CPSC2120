28dc479d96a05bb0a68ff581316767add37f4257
DylanJoey17 <dharve3@clemson.edu>
2024-09-02 19:56:54 -0400

    Removed word count tracking

--- review/review4/review.cpp
+++ review/review4/review.cpp
@@ -2,14 +2,11 @@
 #include <iostream>
 
 void ReadWrite() {
-    int count = 0;
     std::string s1, total = "";
 
     while (std::cin >> s1 && s1 != "q") {
         total = total + s1 + " ";
-        count++;
     }
 
     std::cout << total << std::endl;
-    std::cout << count;
 }
