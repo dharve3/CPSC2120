bcff2ef6a74a20078ac85ee7ccf263d9fc707522
D-Guy Laptop <ddcool535@gmail.com>
2024-09-23 15:45:22 -0400

    Completed circularQueue

--- homework/circularQueue/IntegerArrayQueue.cpp
+++ homework/circularQueue/IntegerArrayQueue.cpp
@@ -1,9 +1,29 @@
 #include "IntegerArrayQueue.h"
 
-bool IntegerArrayQueue::enqueue(int value)
-{
+bool IntegerArrayQueue::enqueue(int value) {
+    // Check if full
+    if ((back + 2) % size == front)
+        return false;
+
+    // Move back to next position
+    back = (back + 1) % size;
+    
+    // Enqueue the value
+    array[back] = value;
+
+    return true;
 }
 
-int IntegerArrayQueue::dequeue()
-{
+int IntegerArrayQueue::dequeue() {
+    // Check if empty
+    if ((back + 1) % size == front)
+        return 0;
+    
+    // Get the value at the front
+    int value = array[front];
+
+    // Move front to next position
+    front = (front + 1) % size;
+
+    return value;
 }
