45cd9a26f57cd01bc1f9dda36901199fed286949
D-Guy Desktop <ddcool535@gmail.com>
2024-09-24 11:04:36 -0400

    Confirmed, comment out main

--- homework/doublyLinked/doublyLinkedSwap.cpp
+++ homework/doublyLinked/doublyLinkedSwap.cpp
@@ -26,30 +26,30 @@ void swapWithNext(Node * p) {
 }
 
 //Be sure to comment out the main() function when submitting to codePost
-int main() {
-    int array[] = {1, 3, 6, 10, 15, 21, 28, 36, 45, 55};
-    Node * head = arrayToList(array, 10);
-    printForwards(head);
-    printBackwards(getTail(head));
+// int main() {
+//     int array[] = {1, 3, 6, 10, 15, 21, 28, 36, 45, 55};
+//     Node * head = arrayToList(array, 10);
+//     printForwards(head);
+//     printBackwards(getTail(head));
     
-    cout << "Swap [0],[1]" << endl;
-    Node * p = getNode(head, 0);
-    swapWithNext(p);
-    printForwards(head);
-    printBackwards(getTail(head));
+//     cout << "Swap [0],[1]" << endl;
+//     Node * p = getNode(head, 0);
+//     swapWithNext(p);
+//     printForwards(head);
+//     printBackwards(getTail(head));
     
-    cout << "Swap [4],[5]" << endl;
-    p = getNode(head, 4);
-    swapWithNext(p);
-    printForwards(head);
-    printBackwards(getTail(head));
+//     cout << "Swap [4],[5]" << endl;
+//     p = getNode(head, 4);
+//     swapWithNext(p);
+//     printForwards(head);
+//     printBackwards(getTail(head));
     
-    cout << "Swap [8],[9]" << endl;
-    p = getNode(head, 8);
-    swapWithNext(p);
-    printForwards(head);
-    printBackwards(getTail(head));
-}
+//     cout << "Swap [8],[9]" << endl;
+//     p = getNode(head, 8);
+//     swapWithNext(p);
+//     printForwards(head);
+//     printBackwards(getTail(head));
+// }
 
 //Do not modify any functions below this line
 Node * arrayToList(int array[], int size)
