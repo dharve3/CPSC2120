7a9147ff9fc75fb52847fb9fc6d5b94be4c70140
D-Guy Desktop <ddcool535@gmail.com>
2024-09-24 11:48:57 -0400

    Wrote calculator

--- homework/postfixCalcStack/calculatePostfixExpression.cpp
+++ homework/postfixCalcStack/calculatePostfixExpression.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <stack>
+#include <algorithm> // find
 
 using namespace std;
 
@@ -23,8 +24,60 @@ using namespace std;
 //for an operation
 //The STL Stack class can be used
 //To easily convert a numeric string to an int you may use the stoi() function
-//which takes a string as a parameter and returns a string
-int calculatePostfixExpression(string expression[], int length)
-{
-    
+//which takes a string as a parameter and returns an int
+int calculatePostfixExpression(string expression[], int length) {
+    if (length == 0) { // if empty
+        return 0;
+    }
+
+    stack<int> operandStack;
+    string operators[] = {"+", "-", "*", "/", "%"};
+
+    for (int i = 0; i < length; i++) {
+        string input = expression[i];
+
+        if (isdigit(input[0])) {
+            // If the input is a number
+            operandStack.push(stoi(input));
+        } else if (find(begin(operators), end(operators), input) != end(operators)) {
+            // Make sure at least 2 operands are in the stack
+            if (operandStack.size() < 2) {
+                return 0;
+            }
+
+            // Pop the top 2 operands
+            int rightOperand = operandStack.top();
+            operandStack.pop();
+            int leftOperand = operandStack.top();
+            operandStack.pop();
+
+            // Preform the operation
+            if (input == operators[0]) { // +
+                operandStack.push(leftOperand + rightOperand);
+            } else if (input == operators[1]) { // -
+                operandStack.push(leftOperand - rightOperand);
+            } else if (input == operators[2]) { // *
+                operandStack.push(leftOperand * rightOperand);
+            } else if (input == operators[3]) { // /
+                if (rightOperand == 0)
+                    return 0; // cannot divide by 0
+
+                operandStack.push(leftOperand / rightOperand);
+            } else if (input == operators[4]) { // %
+                if (rightOperand == 0) 
+                    return 0; // cannot modulo by 0
+
+                operandStack.push(leftOperand % rightOperand);
+            }
+        } else { // Unrecognized input
+            return 0;
+        } // end operator checking
+    } // end of for loop
+
+    // Should only be 1 result remaining
+    if (operandStack.size() == 1) {
+        return operandStack.top();
+    } else {
+        return 0;
+    }
 }
