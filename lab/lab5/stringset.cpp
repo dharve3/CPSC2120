d5e9bd17d6699e375402b78cbb084737e7271849
D-Guy Laptop WSL <ddcool535@gmail.com>
2024-10-09 15:38:13 -0400

    Implement stringset class

--- lab/lab5/stringset.cpp
+++ lab/lab5/stringset.cpp
@@ -25,13 +25,69 @@ int Stringset::getSize() const {
 }
 
 void Stringset::insert(string word) {
-    //Implement this function
+    // if (find(word))
+    //     return; // If word already exists, do nothing
+    // Decided against this implementataion since this hashes the word twice per insert operation
+
+    // Get the hash value for the word
+    unsigned int hashValue = hash<string>{}(word) % size;
+
+    // Check if the word already exists in the linked list at table[hashValue]
+    for (const string& s : table[hashValue]) {
+        if (s == word) {
+            return; // If word already exists, do nothing
+        }
+    }
+
+    // Insert the word into the hash table
+    table[hashValue].push_back(word);
+    num_elems++;
+
+    // Check for resizing
+    if (num_elems == size) {
+        int newSize = size * 2;
+        vector<list<string>> newTable(newSize);
+
+        // Rehash all existing words into the new table
+        for (int i = 0; i < size; i++) {
+            for (const string& s : table[i]) {
+                unsigned int newHashValue = hash<string>{}(s) % newSize;
+                newTable[newHashValue].push_back(s);
+            }
+        }
+
+        // Replace the old table and update size
+        table = newTable;
+        size = newSize;
+    }
+
+
 }
 
 bool Stringset::find(string word) const {
-    //Implement this function
+    // Get the hash value for the word
+    unsigned int hashValue = hash<string>{}(word) % size;
+
+    // Check the linked list at table[hashValue] for the word
+    for (const string &s : table[hashValue]) {
+        if (s == word) {
+            return true; // Word found
+        }
+    }
+
+    return false; // Word not found
 }
 
 void Stringset::remove(string word) {
-    //Implement this function
+    // Get the hash value for the word
+    unsigned int hashValue = hash<string>{}(word) % size;
+
+    // Find and remove the word from the linked list at table[hashValue]
+    for (auto it = table[hashValue].begin(); it != table[hashValue].end(); ++it) {
+        if (*it == word) {
+            table[hashValue].erase(it); // Remove the word
+            num_elems--;
+            return; // No need to continue iterating if word is removed
+        }
+    }
 }
